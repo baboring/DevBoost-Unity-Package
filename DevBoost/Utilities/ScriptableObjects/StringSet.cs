315bf926d5d56142c5fafb3209533e70eed81b36
Benjamin <baboring@gmail.com>
2022-09-25 00:27:58 -0700

    making it unity package

---
+++ DevBoost/Utilities/ScriptableObjects/StringSet.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace ActionBehaviour
+{
+    using NaughtyAttributes;
+
+    [CreateAssetMenu(menuName = "ActionObject/StringSet")]
+    public class StringSet : ScriptableObject
+    {
+        [ReorderableList]
+        [SerializeField]
+        protected List<string> List;
+
+        public List<string>.Enumerator GetEnumerator()
+        {
+            return List.GetEnumerator();
+        }
+
+        public List<string> GetList()
+        {
+            return List;
+        }
+
+    }
+
+
+}
