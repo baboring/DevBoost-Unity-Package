7fa6a74cd0784d177fda517881f21f2434a97faa
Benjamin <baboring@gmail.com>
2022-09-26 22:10:47 -0700

    Revert "sub branch for contirbute"
    This reverts commit b4e92446dd1e5d71a6a4691d2ca3d13db27a9643.

---
+++ Runtime/Behaviours/Base/BaseNode.cs
@@ -0,0 +1,127 @@
+﻿/* *************************************************
+*  Created:  2018-1-28 19:46:40
+*  File:     BaseNode.cs
+*  Author:   Benjamin
+*  Purpose:  []
+****************************************************/
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace ActionBehaviour
+{
+
+    public enum ActionState
+    {
+        None = 0,
+        Success,
+        Fail,
+        Running,
+        Error = -1,
+
+    }
+
+    public interface IBaseNode 
+    {
+        Type agentType { get; }
+        IBaseNode agent { get; }
+    }
+
+    // Base class
+    public abstract class BaseNode : MonoBehaviour,IBaseNode {
+
+        protected ActionState _curr_state;
+        protected ActionState _prev_state;
+
+        public virtual Type agentType { get { return GetType(); } }
+        public IBaseNode agent { get { return this; } }
+        // state
+        public ActionState state {
+            get { return _curr_state; }
+             
+            protected set
+            {
+                if(_curr_state != value) {
+                    _prev_state = _curr_state;
+                    _curr_state = value;
+                    // notify state changed
+                    if (null != onChangedState)
+                        onChangedState(_curr_state,_prev_state);
+                }
+            }
+        }
+
+        // delegates
+        public System.Action<ActionState,ActionState> onChangedState;
+
+
+        // Hierarchy functions
+        protected virtual void OnReset() {}
+
+
+        // Interface methods
+        public void Reset() { 
+            _curr_state = ActionState.None; 
+            _prev_state = ActionState.None; 
+
+            OnReset(); 
+        }
+
+		// ready
+        protected virtual ActionState OnUpdate()
+        {
+            return state == ActionState.None ? ActionState.Success : state;
+        }
+
+		// default core function
+		public virtual ActionState Execute(bool reset = true) {
+
+			// start up 
+            if(reset)
+                OnReset();
+
+			// update 
+		    state = OnUpdate();
+
+			return state;
+		}
+
+        public void ExecuteInvoke()
+        {
+            Execute();
+        }
+    }
+
+    // Aync proc
+    public class AsyncProcessing
+    {
+        public event System.Action<AsyncProcessing> onCompleted;
+        bool _isDone;
+
+        // properties
+        public bool isDone
+        {
+            get
+            {
+                return _isDone;
+            }
+            set
+            {
+                _isDone = value;
+                if (_isDone && null != onCompleted)
+                {
+                    onCompleted(this);
+                    backgroundWorking = null;
+                }
+            }
+        }
+
+        public Coroutine backgroundWorking;
+        public AsyncProcessing(Coroutine backgroundWorking)
+        {
+            this.backgroundWorking = backgroundWorking;
+            _isDone = false;
+        }
+    }
+
+}
