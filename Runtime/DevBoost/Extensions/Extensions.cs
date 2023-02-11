64f26f95548f992a639a4945121a9fd775a5fd89
Benjamin <baboring@gmail.com>
2023-02-10 23:58:44 -0800

    added camera extension

--- Runtime/DevBoost/Extensions/Extensions.cs
+++ Runtime/DevBoost/Extensions/Extensions.cs
@@ -120,6 +120,58 @@ namespace DevBoost
         #endregion
     }
 
+    public static class CameraExtensions
+    {
+        public static void LayerCullingShow(this Camera cam, int layerMask)
+        {
+            cam.cullingMask |= layerMask;
+        }
+        public static void LayerCullingShow(this Camera cam, string layer)
+        {
+            LayerCullingShow(cam, 1 << LayerMask.NameToLayer(layer));
+        }
+        public static void LayerCullingHide(this Camera cam, int layerMask)
+        {
+            cam.cullingMask &= ~layerMask;
+        }
+        public static void LayerCullingHide(this Camera cam, string layer)
+        {
+            LayerCullingHide(cam, 1 << LayerMask.NameToLayer(layer));
+        }
+        public static void LayerCullingToggle(this Camera cam, int layerMask)
+        {
+            cam.cullingMask ^= layerMask;
+        }
+        public static void LayerCullingToggle(this Camera cam, string layer)
+        {
+            LayerCullingToggle(cam, 1 << LayerMask.NameToLayer(layer));
+        }
+        public static bool LayerCullingIncludes(this Camera cam, int layerMask)
+        {
+            return (cam.cullingMask & layerMask) > 0;
+        }
+        public static bool LayerCullingIncludes(this Camera cam, string layer)
+        {
+            return LayerCullingIncludes(cam, 1 << LayerMask.NameToLayer(layer));
+        }
+        public static void LayerCullingToggle(this Camera cam, int layerMask, bool isOn)
+        {
+            bool included = LayerCullingIncludes(cam, layerMask);
+            if (isOn && !included)
+            {
+                LayerCullingShow(cam, layerMask);
+            }
+            else if (!isOn && included)
+            {
+                LayerCullingHide(cam, layerMask);
+            }
+        }
+        public static void LayerCullingToggle(this Camera cam, string layer, bool isOn)
+        {
+            LayerCullingToggle(cam, 1 << LayerMask.NameToLayer(layer), isOn);
+        }
+    }
+
     public static class ScrollRectExtensions
     {
         public static void SnapTo(this UnityEngine.UI.ScrollRect scrollRect, RectTransform contentPanel, RectTransform target)
