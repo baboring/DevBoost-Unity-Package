46c1e73997edb01ac06d52338e986de11087921f
Benjamin <baboring@gmail.com>
2023-03-01 13:14:18 -0800

    added Excample class

---
+++ Samples/Examples.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Examples : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DevBoost.Utilities.CsvUtilArrayHelper.AddCustumTypeDescriptorProvider();
+
+    }
+
+
+}
