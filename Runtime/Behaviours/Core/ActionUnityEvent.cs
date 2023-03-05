3be8785e93799b0e649d4a5f4f91176a541cbe7b
Benjamin <baboring@gmail.com>
2023-03-05 00:30:51 -0800

    added ActionUnityEvent

---
+++ Runtime/Behaviours/Core/ActionUnityEvent.cs
@@ -0,0 +1,31 @@
+﻿/* *************************************************
+*  Created:  2023-3-05 22:46:32
+*  File:     ActionUnityEvent.cs
+*  Author:   Benjamin
+*  Purpose:  []
+****************************************************/
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DevBoost.ActionBehaviour {
+
+	public class ActionUnityEvent : ActionNode
+	{
+		[SerializeField]
+		protected UnityEvent events = new UnityEvent();
+
+		protected override ActionState OnUpdate()
+		{
+
+			// parent update
+			ActionState result = base.OnUpdate();
+			if (result != ActionState.Success)
+				return result;
+
+			events.Invoke();
+
+			return ActionState.Success;
+		}
+	}
+}
