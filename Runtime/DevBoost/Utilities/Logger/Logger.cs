92b92bc9bc3c5923ee0915df7650f0c04398c6b1
baboring <baboring@gmail.com>
2023-04-23 21:33:31 -0700

    added logger for log4net

---
+++ Runtime/DevBoost/Utilities/Logger/Logger.cs
@@ -0,0 +1,34 @@
+/* *************************************************
+*  Created:  2023-2-23 14:51:00
+*  Author:   Benjamin
+*  Purpose:  []
+****************************************************/
+
+#if USE_LOG4NET
+
+using System.IO;
+using log4net;
+using log4net.Config;
+
+
+namespace DevBoost.Utilities
+{
+    public static class Logger
+    {
+        static public ILog Dev { get; private set; }
+
+        static public bool Init(FileInfo fileInfo)
+        {
+            if ((fileInfo?.Length ?? -1) != -1)
+            {
+                XmlConfigurator.Configure(fileInfo);
+                Dev = log4net.LogManager.GetLogger("Dev");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
+#endif
