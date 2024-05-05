24718135e01adf7a9d7d0483123d532dfedbffb0
Benjamin Park <baboring@gmail.com>
2024-05-05 00:40:58 -0700

    Updated Prefab Group logic

--- Runtime/DevBoost/Utilities/PrefabGroup.cs
+++ Runtime/DevBoost/Utilities/PrefabGroup.cs
@@ -18,51 +18,210 @@ namespace DevBoost.Utilities
     [CreateAssetMenu(fileName = "PrefabGroup", menuName = "Resources/PrefabGroup", order = 1)]
     public class PrefabGroup : ScriptableObject
     {
+        [SerializeField] private string label = "prefab";
         // list of items
-        [SerializeField] private List<Chunk> m_Prefabs;           // list of prefabs
+        [SerializeField] private List<Chunk> lstPrefabs;           // list of prefabs
 
-        public IEnumerable<Chunk> List { get => m_Prefabs; }
+        [SerializeField] private List<PrefabGroup> fallbackGroups;
 
-        public GameObject Find(string label)
+        public IEnumerable<Chunk> List { get => lstPrefabs; }
+        public string Label => label;
+
+        public List<PrefabGroup> GetAllGroups()
         {
-            var find = m_Prefabs.FirstOrDefault(va => va.label == label);
-            if (find == default(Chunk))
+            var lst = new List<PrefabGroup>() { this };
+            FetchFallbackList(lst);
+            return lst;
+        }
+
+        private void FetchFallbackList(List<PrefabGroup> builder)
+        {
+            foreach (var group in fallbackGroups)
+            {
+                if (!builder.Contains(group))
+                {
+                    builder.Add(group);
+                    group.FetchFallbackList(builder);
+                }
+            }
+        }
+
+        /// <summary>
+        /// find the prefabgroup asset recursively
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        private PrefabGroup FindGroupList(string groupName, List<PrefabGroup> builder)
+        {
+            if (builder.Contains(this))
                 return null;
-            return find.prefab;
+
+            builder.Add(this);
+
+            if (label == groupName)
+                return this;
+
+            foreach (var group in fallbackGroups)
+            {
+                var found = group.FindGroupList(groupName, builder);
+                if (found != null)
+                    return found;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Find the prefab group by the label
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public bool TryGetGroup(string groupName, out PrefabGroup prefabGroup)
+        {
+            if (label == groupName)
+            {
+                prefabGroup = this;
+                return this;
+            }
+
+            prefabGroup = FindGroupList(groupName, new List<PrefabGroup>());
+            return prefabGroup != null;
+        }
+
+        /// <summary>
+        /// Find the Prefab GameObject by the label
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="useFallback"></param>
+        /// <returns></returns>
+        public GameObject Find(string label, bool useFallback = true)
+        {
+            return FindChunk(label, useFallback)?.prefab;
+        }
+
+        /// <summary>
+        /// Find the Prefab Chunk by the label
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="useFallback"></param>
+        /// <returns></returns>
+        public Chunk FindChunk(string label, bool useFallback = true)
+        {
+            var find = lstPrefabs.FirstOrDefault(va => va.label == label);
+            if (find == default(Chunk))
+            {
+                if (useFallback)
+                {
+                    foreach (var group in GetAllGroups())
+                    {
+                        if (group == this)
+                            continue;
+                        var obj = group.FindChunk(label, false);
+                        if (obj != null)
+                            return obj;
+                    }
+                }
+                return null;
+            }
+            return find;
+        }
+
+        /// <summary>
+        /// Find a chunk of the prefab in the list
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="group"></param>
+        /// <param name="found"></param>
+        /// <param name="useFallback"></param>
+        /// <returns></returns>
+        public bool TryFindChunk(string label, out PrefabGroup group, out GameObject found, bool useFallback = true)
+        {
+            group = this;
+            found = null;
+            if (TryFindChunk(label, out group, out Chunk chunk, useFallback))
+                found = chunk.prefab;
+            return found != null;
+        }
+
+        public bool TryFindChunk(string label, out PrefabGroup group, out Chunk found, bool useFallback = true)
+        {
+            group = this;
+            found = null;
+            var find = lstPrefabs.FirstOrDefault(va => va.label == label);
+            if (find == default(Chunk))
+            {
+                if (useFallback)
+                {
+                    foreach (var _group in GetAllGroups())
+                    {
+                        if (_group == this)
+                            continue;
+                        var obj = _group.FindChunk(label, false);
+                        if (obj != null)
+                        {
+                            found = obj;
+                            group = _group;
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            found = find;
+            return true;
+        }
+
+        public List<GameObject> SpawnAll(Transform parant = null, SpawnOption options = SpawnOption.None, bool useFallback = true)
+        {
+            List<GameObject> spawned = new List<GameObject>();
+            if (!useFallback)
+            {
+                foreach (var prefab in List)
+                    spawned.Add(prefab.InstantiatePrefab(parant, options, label));
+            }
+            else
+            {
+                foreach (var group in GetAllGroups())
+                    spawned.AddRange(group.SpawnAll(parant, options, false));
+            }
+            return spawned;
+        }
+
+        /// <summary>
+        /// Spawn the GameObject by the name
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="parant"></param>
+        /// <param name="options"></param>
+        /// <param name="useFallback"></param>
+        /// <returns></returns>
+        public GameObject Spawn(string label, Transform parant = null, SpawnOption options = SpawnOption.None, bool useFallback = true)
+        {
+            return Spawn<GameObject>(label, parant, options, useFallback);
         }
 
 
-        public GameObject Spawn(string label, Transform parant, SpawnOption options = SpawnOption.None)
+        public T Spawn<T>(string label, Transform parant = null, SpawnOption options = SpawnOption.None, bool useFallback = true) where T : class
         {
-            return Spawn<GameObject>(label, parant, options);
-        }
-
-
-        public T Spawn<T>(string label, Transform parant, SpawnOption options = SpawnOption.None) where T : class
-        {
-            var find = Find(label);
-            if (find == null)
+            if (!TryFindChunk(label, out PrefabGroup group, out Chunk find, useFallback))
                 return default(T);
 
-            var spawned = Instantiate(find, parant);
-            spawned.SetActive(true);
-            spawned.name = $"{name}:{label}";// :: {spawned.name.Replace("(Clone)","")}";
-            if ((options & SpawnOption.LocalPosZero) == SpawnOption.LocalPosZero) spawned.transform.localPosition = Vector3.zero;
-            if ((options & SpawnOption.LocalScaleOne) == SpawnOption.LocalScaleOne) spawned.transform.localScale = Vector3.one;
+            var spawned = find.InstantiatePrefab(parant, options, group.label);
 
             if (typeof(T) == typeof(GameObject))
                 return spawned as T;
             return spawned.GetComponent<T>();
         }
 
-    #if UNITY_EDITOR
+#if UNITY_EDITOR
+        //[EasyButtons.Button("AutoBuild")]
         public void AutoBuild()
         {
             string path = EditorUtility.OpenFolderPanel("Select a folder", "Assets", "");
             if (path.Contains(Application.dataPath))
             {
                 var allPrefabs = GetAssets<GameObject>(new string[] { "Assets" + path.Substring(Application.dataPath.Length) }, "t:prefab");
-                m_Prefabs = allPrefabs.Select(va => new Chunk() { label = va.name, prefab = va.gameObject }).ToList();
+                lstPrefabs = allPrefabs.Select(va => new Chunk() { label = va.name, prefab = va.gameObject }).ToList();
 
                 EditorUtility.SetDirty(this);
             }
@@ -83,10 +242,11 @@ namespace DevBoost.Utilities
         /// <summary>
         /// Verify Sprites
         /// </summary>
-        public void VerifySprites()
+        //[EasyButtons.Button("Verify links")]
+        public void VerifyLinks()
         {
             Debug.Log($"Verify : {name} -----------------------------------------------");
-            foreach (var item in m_Prefabs)
+            foreach (var item in lstPrefabs)
             {
                 if (item.prefab == null)
                 {
@@ -97,16 +257,31 @@ namespace DevBoost.Utilities
                     Debug.Log($"{item.label} - Ok");
                 }
             }
-            Debug.Log($"Total {m_Prefabs.Count} -----------------------------------------------");
+            Debug.Log($"Total {lstPrefabs.Count} -----------------------------------------------");
         }
-    #endif
+#endif
 
         [System.Serializable]
         public class Chunk
         {
             public string label;
             public GameObject prefab;
-        }
 
+            public GameObject InstantiatePrefab(Transform parant = null, SpawnOption options = SpawnOption.None, string groupLabel = "")
+                => InstantiatePrefab<GameObject>(parant, options, groupLabel);
+
+            public T InstantiatePrefab<T>(Transform parant = null, SpawnOption options = SpawnOption.None, string groupLabel = "") where T : class
+            {
+                var spawned = Object.Instantiate(prefab, parant);
+                spawned.SetActive(true);
+                spawned.name = $"{groupLabel}:{label}";// :: {spawned.name.Replace("(Clone)","")}";
+                if ((options & SpawnOption.LocalPosZero) == SpawnOption.LocalPosZero) spawned.transform.localPosition = Vector3.zero;
+                if ((options & SpawnOption.LocalScaleOne) == SpawnOption.LocalScaleOne) spawned.transform.localScale = Vector3.one;
+
+                if (typeof(T) == typeof(GameObject))
+                    return spawned as T;
+                return spawned.GetComponent<T>();
+            }
+        }
     }
 }
